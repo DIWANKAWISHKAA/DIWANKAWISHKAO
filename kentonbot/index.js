8bff3403f0231de0766ad3d7ea9fd108916c3ff9
BlackTiger <99858457+DIWANKAWISHKAA@users.noreply.github.com>
2022-02-22 16:49:36 +0530

    Create index.js

---
+++ kentonbot/index.js
@@ -0,0 +1,10 @@
+//https://github.com/DIWANKAWISHKAA/KENTONBOT
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
